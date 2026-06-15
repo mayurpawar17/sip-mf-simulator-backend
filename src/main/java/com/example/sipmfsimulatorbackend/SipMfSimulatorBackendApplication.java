819ee9b669831a003b00ed49a4ad7f85aa9a9878
mayurpkapilit23 <mayurp@kapilit.com>
2026-06-15 18:24:09 +0530

    project initiated

---
+++ src/main/java/com/example/sipmfsimulatorbackend/SipMfSimulatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sipmfsimulatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipMfSimulatorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SipMfSimulatorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sipmfsimulatorbackend/SipMfSimulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sipmfsimulatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SipMfSimulatorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
